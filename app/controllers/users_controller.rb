df8c1a69e1134477e9130612b6d5ab4c7b055d09
Abdusaid10 <Abdusaid10@github.com>
2019-08-31 11:35:49 +0500

    add admin and authorize it with destroi action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
-  
+  before_action :admin_user, only: :destroy
   def index
     @users = User.paginate(page: params[:page])
   end
@@ -39,6 +39,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    user=User.find(params[:id]).destroy
+    flash[:success]="User #{user.name} deleted"
+    redirect_to users_url
+  end
+
   private 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
@@ -60,4 +66,7 @@ class UsersController < ApplicationController
     redirect_to(root_url) unless current_user?(@user)
   end
 
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
