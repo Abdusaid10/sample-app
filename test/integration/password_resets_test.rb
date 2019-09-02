1683a16790c200642300dfc616e400ba0fd506c1
Abdusaid10 <Abdusaid10@github.com>
2019-09-02 17:47:05 +0500

    fixing errors in password resets test

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -7,10 +7,10 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
   end
 
   test "password resets" do
-    get new_password_resets_path
+    get new_password_reset_path
     assert_template 'password_resets/new'
     # invalid email
-    post new_password_resets_path, params: { password_reset: { email: "" }}
+    post password_resets_path, params: { password_reset: { email: "" }}
     assert_not flash.empty?
     assert_template "password_resets/new"
     # Valid email
@@ -20,7 +20,7 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to root_url
     # Password reset form
-    user- assigns(:user)
+    user = assigns(:user)
     # wrong email
     get edit_password_reset_path(user.reset_token, email: "")
     assert_redirected_to root_url
@@ -37,7 +37,7 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     assert_template 'password_resets/edit'
     assert_select "input[name=email][type=hidden][value=?]", user.email
     # Invalid password & confirmation
-    patch password_rest_path(user.reset_token), 
+    patch password_reset_path(user.reset_token), 
           params: { email: user.email, user: { password: "foobaz", password_confirmation: "barquux" }}
     assert_select 'div#error_explanation'
     # Empty password 
@@ -48,9 +48,9 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # Valid password & confirmation
     patch password_reset_path(user.reset_token),
-    params: { email: user.email,
-          user: { password:              "foobaz",
-                  password_confirmation: "foobaz" } }
+          params: { email: user.email,
+                user: { password:              "foobaz",
+                        password_confirmation: "foobaz" } }
     assert is_logged_in?
     assert_not flash.empty?
     assert_redirected_to user
