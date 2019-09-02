bdcf4a6aede70afad295ff939752d176ee6e8e43
Abdusaid10 <Abdusaid10@github.com>
2019-09-02 15:26:21 +0500

    add password reset method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # User model
 class User < ApplicationRecord
   # attribute accessors
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
 
@@ -49,6 +49,16 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+    
   private
   def downcase_email
     self.email = email.downcase
