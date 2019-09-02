ddce949fe171fcb28a4e002f7a536c32a4267dae
Abdusaid10 <Abdusaid10@github.com>
2019-09-02 09:41:43 +0500

    generalizing the authenticated? method

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,13 +31,22 @@ class User < ApplicationRecord
 
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
-    return false if remember_digest.nil?
+    return false if digest.nil?
     # BCrypt::Password.new(remember_digest).is_password?(remember_token)
     BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 
   private
