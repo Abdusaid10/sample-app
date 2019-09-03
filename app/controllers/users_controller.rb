9aeb117444d6541fe187ea1b171e7dc8c4f0ff00
Abdusaid10 <Abdusaid10@github.com>
2019-09-03 09:33:22 +0500

    add microposts creation to Home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
   def index
@@ -58,13 +58,13 @@ class UsersController < ApplicationController
   # Before filters
 
   # Confirm a logged-in user
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger]="Please log in"
-      redirect_to login_url
-    end
-  end
+  # def logged_in_user
+  #   unless logged_in?
+  #     store_location
+  #     flash[:danger]="Please log in"
+  #     redirect_to login_url
+  #   end
+  # end
 
   def correct_user
     @user = User.find(params[:id])
